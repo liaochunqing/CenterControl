45792769b6498f1b0c9bc0c38878c82238b29efc
liaochunqing <610109289@qq.com>
2022-11-22 18:52:34 +0800

    删除多余代码

--- appiot/APGroupTableView.h
+++ appiot/APGroupTableView.h
@@ -12,17 +12,15 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface APGroupTableView : UITableView<UITableViewDelegate,UITableViewDataSource,UISearchResultsUpdating,UISearchBarDelegate>
+@interface APGroupTableView : UITableView
 @property (nonatomic , strong) NSMutableArray *data;//传递过来已经组织好的数据（全量数据）
 
 @property (nonatomic , strong) NSMutableArray *tempData;//用于存储数据源（部分数据）
 @property (nonatomic,strong)UISearchController *searchController;//搜索
 @property (nonatomic,strong)NSMutableArray *filteredData;//存储搜索过滤后的数据
 
-
-
 //-(NSMutableArray *)createTempData : (NSArray *)data;
--(void)selectedAllWithSelected:(BOOL)selected;
+//-(void)selectedAllWithSelected:(BOOL)selected;
 -(void)deleteSelectedNode;
 @end
 
